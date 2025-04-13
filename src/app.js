cca6b470ac9c9ef31e5249c10ea8ceef2e717b4e
Grin <martanvcs@gmail.com>
2025-04-13 22:31:10 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+ const suits = ["heart", "spade", "club", "diamond"];
+  const symbols = {
+    heart: "♥",
+    spade: "♠",
+    club: "♣",
+    diamond: "♦"
+  };
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const suitIndex = Math.floor(Math.random() * 4);
+  const valueIndex = Math.floor(Math.random() * 13);
+
+  const suit = suits[suitIndex];
+  const value = values[valueIndex];
+  const symbol = symbols[suit];
+
+  const card = document.getElementById("card");
+
+  card.classList.add("animate");
+
+  setTimeout(() => {
+    card.className = "card " + suit;
+    card.querySelector(".top").innerHTML = `${value} ${symbol}`;
+    card.querySelector(".middle").innerHTML = `${symbol}`;
+    card.querySelector(".bottom").innerHTML = `${value} ${symbol}`;
+
+    
+    card.classList.remove("animate");
+  }, 300); 
+}
+
+
